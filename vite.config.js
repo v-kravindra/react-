0595822be67566387456429e515483df14dfc4e7
v-kravindra <162555898+v-kravindra@users.noreply.github.com>
2024-04-05 21:42:42 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import EnvironmentPlugin from 'vite-plugin-environment';
+import eslint from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths(), eslint(),  EnvironmentPlugin('all')], //eslint(),
+  resolve: {
+    alias: {
+      '~uswds': path.resolve(__dirname, 'node_modules/@uswds/uswds'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['node_modules/@uswds/uswds/packages'],
+      },
+    },
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+  server: {
+    open: true,
+    port: 8080,
+    hmr: {
+      overlay: false
+    },
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
